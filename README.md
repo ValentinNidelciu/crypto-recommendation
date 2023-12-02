af6d1c10888295eacb1be3c2f92e8f15e8dd70d7
valentin <valentin.nidelciu@gmail.com>
2023-12-02 19:29:13 +0200

    Added swagger documentation. Updated .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # crypto-recommendation
+
+Endpoints documentation was realised using OpenAPI 3 and SwaggerUI. You can check it out by accessing: http://localhost:8080/swagger-ui/index.html#/ 
--- src/main/java/ro/assignment/cryptorec/exposition/CryptoPriceRestController.java
+++ src/main/java/ro/assignment/cryptorec/exposition/CryptoPriceRestController.java
@@ -1,10 +1,12 @@
 package ro.assignment.cryptorec.exposition;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.assignment.cryptorec.application.CryptoPriceService;
-import ro.assignment.cryptorec.domain.CryptoPrice;
 import ro.assignment.cryptorec.exposition.views.CryptoPriceView;
 import ro.assignment.cryptorec.exposition.views.HighestNormalizedRangeView;
 import ro.assignment.cryptorec.exposition.views.NormalizedRangeView;
@@ -19,43 +21,95 @@ public class CryptoPriceRestController {
     private CryptoPriceService cryptoPriceService;
 
 
+    @Operation(summary = "Import/ Reimport the data from the static .csv files")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The import was successful"),
+            @ApiResponse(responseCode = "500", description = "The import could not be realised due to a server error")
+    })
     @PostMapping("/import")
     public ResponseEntity<Void> importPrices() {
         this.cryptoPriceService.importPrices();
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Fetch all the price entries for all available symbols")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the prices for all available symbols"),
+            @ApiResponse(responseCode = "500", description = "A server error has occurred")
+    })
     @GetMapping
     public ResponseEntity<List<CryptoPriceView>> getALl() {
         return ResponseEntity.ok(CryptoPriceView.toView(this.cryptoPriceService.getAll()));
     }
 
+    @Operation(summary = "Fetch the oldest price record available for a specific symbol")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The oldest price was found"),
+            @ApiResponse(responseCode = "500", description = "A server error has occurred"),
+            @ApiResponse(responseCode = "400", description = "An invalid request param has been received")
+    })
     @GetMapping("/oldest")
     public ResponseEntity<CryptoPriceView> getOldestPrice(final @RequestParam("symbol") String symbol) {
         return ResponseEntity.ok(CryptoPriceView.toView(this.cryptoPriceService.getOldestPrice(symbol)));
     }
 
+
+    @Operation(summary = "Fetch the newest price record available for a specific symbol")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The newest price was found"),
+            @ApiResponse(responseCode = "500", description = "A server error has occurred"),
+            @ApiResponse(responseCode = "400", description = "An invalid request param has been received")
+    })
     @GetMapping("/newest")
     public ResponseEntity<CryptoPriceView> getNewestPrice(final @RequestParam("symbol") String symbol) {
         return ResponseEntity.ok(CryptoPriceView.toView(this.cryptoPriceService.getNewestPrice(symbol)));
     }
 
+
+    @Operation(summary = "Fetch the minimum price record available for a specific symbol")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The minimum price was found"),
+            @ApiResponse(responseCode = "500", description = "A server error has occurred"),
+            @ApiResponse(responseCode = "400", description = "An invalid request param has been received")
+    })
     @GetMapping("/min-price")
     public ResponseEntity<CryptoPriceView> getMinPrice(final @RequestParam("symbol") String symbol) {
         return ResponseEntity.ok(CryptoPriceView.toView(this.cryptoPriceService.getMinPrice(symbol)));
     }
 
+
+    @Operation(summary = "Fetch the maximum price record available for a specific symbol")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The maximum price was found"),
+            @ApiResponse(responseCode = "500", description = "A server error has occurred"),
+            @ApiResponse(responseCode = "400", description = "An invalid request param has been received")
+    })
     @GetMapping("/max-price")
     public ResponseEntity<CryptoPriceView> getMaxPrice(final @RequestParam("symbol") String symbol) {
         return ResponseEntity.ok(CryptoPriceView.toView(this.cryptoPriceService.getMaxPrice(symbol)));
 
     }
 
+
+
+    @Operation(summary = "Fetch the highest normalized range value for all symbols for a specific date.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The highest normalized range value was found for the specific date"),
+            @ApiResponse(responseCode = "500", description = "A server error has occurred"),
+            @ApiResponse(responseCode = "400", description = "An invalid request param has been received")
+    })
     @GetMapping("/highest-normalized-range")
     public ResponseEntity<HighestNormalizedRangeView> getHighestNormalizedRange(final @RequestParam("date") LocalDate date) {
         return ResponseEntity.ok(this.cryptoPriceService.getHighestNormalizedRangeForDate(date));
     }
 
+
+
+    @Operation(summary = "Fetch the descending sorted list of normalized range values for all symbols")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The list was computed successfully"),
+            @ApiResponse(responseCode = "500", description = "A server error has occurred")
+    })
     @GetMapping("/normalized-range-list")
     public ResponseEntity<List<NormalizedRangeView>> getAllSortedDescendingByNormalizedRange() {
         return ResponseEntity.ok(this.cryptoPriceService.getAllSortedDescendingByNormalizedRange());
